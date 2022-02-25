d4c2d1a1def2fbb73c698aa0e743b76c83625f8f
shaz <1992shaz@gmail.com>
2022-02-25 15:54:44 +0530

    imported & queried model into views

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord, Webpage, Topic
 
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me': "I'm from views.py"}
-    return render(request, 'first_app/index.html')
+    webpage_lsit = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpage_lsit}
+    return render(request, 'first_app/index.html', context=date_dict)
