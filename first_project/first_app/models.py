51a8678d9d4e4fdfe1aafa3fdf0dcbab26d386ab
shaz <1992shaz@gmail.com>
2022-02-25 13:49:59 +0530

    models created

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,27 @@
+from distutils.archive_util import make_zipfile
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self) -> str:
+        return self.top_name
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.date
+
